cde529dcdcefb8baaece085e824865fc9fd713e9
neildan <felipeluiz152000@gmail.com>
2022-07-06 20:06:45 -0500

    Se inicia proyecto Vuejs, con bootstrap 5.1. Se toma como base los audios e imagenes que proporciono NextU

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # nextu.oye.music
 Proyecto de NextU: OYE es un sitio para escuchar música gratis
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <header>
+    <Navbar msg="You did it!" />
+  </header>
+
+  <main class="container">
+    <h1>Hola mundo</h1>
+
+    <div class="alert alert-success" role="alert">
+      A simple success alert—check it out!
+    </div>
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
